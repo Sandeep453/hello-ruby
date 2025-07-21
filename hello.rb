4a0c532ee9a9d741c09c622c0764dd04575bcb85
EC2 Default User <ec2-user@ip-172-31-7-59.ap-south-1.compute.internal>
2025-07-21 12:54:42 +0000

    second commit

--- hello.rb
+++ hello.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, Ruby app running on docker by using jenkins and git as CI CD!"
+  "Hello, Ruby app running on docker by using jenkins and git as CI CD - testing !"
 end
